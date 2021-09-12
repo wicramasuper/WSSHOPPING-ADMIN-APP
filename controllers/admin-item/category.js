2704e695368fa3b42c5338f996f172b90776b93f
kavindamai99 <kavindanim@gmail.com>
2021-09-12 14:02:50 +0530

    Category Backend

---
+++ controllers/admin-item/category.js
@@ -0,0 +1,53 @@
+const Category = require("../../models/admin-item/category");
+//error handler in helper folder
+const { errorHandler } = require('../../helpers/admin-item');
+
+exports.insertCategory=(req, res) =>{
+    const category = new Category(req.body);
+
+
+    
+    category.save((err,data)=>{
+        if(err){
+            return res.status(400).json({
+
+                error:errorHandler(err)
+            });
+
+        }
+        res.json({data});
+    })
+
+
+
+}
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: 'Category does not exist'
+            });
+        }
+        req.category = category;
+        next();
+    });
+};
+
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+};
+
+
+//fetch category list
+exports.list = (req, res) => {
+    Category.find().exec((err, data) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json(data);
+    });
+};
